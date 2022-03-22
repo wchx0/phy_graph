b0909fefc327de1023ed1ed2e588634636978586
wchx0 <wcxwlclz@163.com>
2022-03-22 12:20:50 +0800

    normalize the configuration and employ matploylib.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import phyplot as pp
 
 # 读取配置文件
-conf_path = './phy_graph.conf'
-info = pp.read_conf(conf_path)
+conf_path = './config.ini'
+conf, data = pp.read_conf(conf_path)
 
 # 计算方程表达式
-reg = pp.calculate(info)
+reg = pp.calculate(data)
 
 # 作图
 fig_path = './graph/'
-pp.draw(info, reg, fig_path)
+pp.draw(conf, data, reg, fig_path)
--- phyplot.py
+++ phyplot.py
@@ -1,7 +1,8 @@
+from email.policy import default
 import os
-import time
 import math
 import shutil
+import configparser
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -9,63 +10,68 @@ import matplotlib.offsetbox as msb
 
 
 def read_conf(path):
-    '''传入配置文件路径，返回配置文件中的变量的字典。'''
-    try:
-        with open(path, encoding = 'utf-8') as conf:
-            exec(conf.read())
-    except:
-        print('配置文件"phyplot.conf"损坏！\n请正确设置配置文件后重试。')
-        input('按<Enter>退出。')
+    '''传入配置文件路径；
+以ConfigParser类返回配置文件中的变量。'''
+    conf = configparser.ConfigParser()
+    conf.read(path, encoding = 'utf-8')
+    data = {
+    'x':np.float64(conf.get('data', 'x').replace(' ', '').split(',')),
+    'y':np.float64(conf.get('data', 'y').replace(' ', '').split(','))
+    }
+    conf.set('caption', 'text', '')
 
-    info = vars()
+    x_data = data['x']
 
-    return info 
+    return conf, data
 
-def calculate(info):
-    '''传入配置文件字典，返回线性回归方程的参数。'''
-    x_data = info['x_data']
-    y_data = info['y_data']
 
-    n = len(x_data)
-    if n != len(y_data) or n < 3:
+def calculate(data):
+    '''传入配置文件字典；
+返回线性回归方程的参数。'''
+    x_data = data['x']
+    y_data = data['y']
+
+    n = np.size(x_data)
+    if n != np.size(y_data) or n < 3:
         raise Exception
 
-    x_sum = sum(x_data)
-    y_sum = sum(y_data)
+    x_sum = x_data.sum()
+    y_sum = y_data.sum()
     x_average = x_sum / n
     y_average = y_sum / n
-    xx_sum = sum((x * x for x in x_data))
-    xy_sum = sum((x * y for (x, y) in zip(x_data, y_data)))
+    xx_sum = np.square(x_data).sum()
+    xy_sum = (x_data * y_data).sum()
     l_xx = xx_sum - x_sum * x_sum / n
     l_xy = xy_sum - x_sum * y_sum / n
     k = l_xy / l_xx
     b = y_average - k * x_average
     r = l_xy / math.sqrt(l_xx * l_xy)
     
-    y_pre = [k * x + b for x in x_data]
-    y_dev = [y_data[i] - y_pre[i] for i in range(n)]
-    y_dev_square_sum = sum((dev * dev for dev in y_dev))
+    y_pre = k * x_data + b
+    y_dev = y_data - y_pre
+    y_dev_square_sum = np.square(y_dev).sum()
     s_y = math.sqrt(y_dev_square_sum / (n - 2))
     s_k = s_y / math.sqrt(l_xx)
     s_b = s_k * math.sqrt(xx_sum / n)
 
-    reg = vars()
+    reg = {'k':k, 'b':b, 's_k':s_k, 's_b':s_b, 'y_pre':y_pre}
 
     return reg
 
 
-def draw(info, reg, path):
-    '''传入配置文件字典、参数字典、图像保存路径，作图并保存在特定路径。'''
+def draw(conf, data, reg, path):
+    '''传入配置文件字典、参数字典、图像保存路径；
+作图并保存在特定路径。'''
     plt.rcParams['font.sans-serif'] = ['SimHei']
     plt.figure()
     plt.grid(True)
-    plt.scatter(info['x_data'], info['y_data'], color = 'black')
-    plt.plot(info['x_data'], reg['y_pre'], color = 'black')
-    plt.xlabel(info['x_label'])
-    plt.ylabel(info['y_label'])
-    plt.text(4.2, 2, s = info['text'])
+    plt.scatter(data['x'], data['y'], color = 'black')
+    plt.plot(data['x'], reg['y_pre'], color = 'black')
+    plt.xlabel(conf.get('caption', 'x_label'))
+    plt.ylabel(conf.get('caption', 'y_label'))
+    plt.text(4.2, 2, s = conf.get('caption', 'text'))
 
-    fig_name = '{}.png'.format(time.strftime('%Y%m%d_%H%M%S', time.localtime()))
+    fig_name = '{}.png'.format(conf.get('caption', 'title'))
     try:
         plt.savefig(path + fig_name)
     except FileNotFoundError:
